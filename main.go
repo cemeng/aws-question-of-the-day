bfa2ab66893d0f10d987fc810977645d9f376c90
Felix Tjandrawibawa <cemeng@gmail.com>
2018-02-17 23:36:21 +1100

    Clean up

--- main.go
+++ main.go
@@ -11,11 +11,6 @@ import (
 	"log"
 )
 
-var (
-	ErrNameNotProvided = errors.New("no name was provided in the HTTP body")
-	ErrRetrievingItem  = errors.New("error retrieving item")
-)
-
 type Item struct {
 	Question string `json:"question"`
 	Answer   string `json:"answer"`
@@ -24,22 +19,8 @@ type Item struct {
 func Handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
 
 	// stdout and stderr are sent to AWS CloudWatch Logs
-	log.Printf("Processing Lambda request %s\n", request.RequestContext.RequestID)
+	// log.Printf("Processing Lambda request %s\n", request.RequestContext.RequestID)
 
-	// If no name is provided in the HTTP request body, throw an error
-	if len(request.Body) < 1 {
-		return events.APIGatewayProxyResponse{}, ErrNameNotProvided
-	}
-
-	// Do a dynamoDB look up
-	// return on the body result
-	// Python code
-	// dynamodb = boto3.resource('dynamodb')
-	// table = dynamodb.Table(os.environ['DB_TABLE_NAME'])
-	// items = table.scan()
-	// items = table.query(
-	//     KeyConditionExpression=Key('id').eq(postId)
-	// )
 	sess, err := session.NewSession(&aws.Config{
 		Region: aws.String("ap-southeast-2")},
 	)
@@ -55,14 +36,15 @@ func Handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyRespo
 
 	if err != nil {
 		log.Printf("Error retrieving from dynamoDB", err.Error())
+		var ErrRetrievingItem = errors.New("Error retrieving item " + err.Error())
 		return events.APIGatewayProxyResponse{}, ErrRetrievingItem
 	}
-	item := Item{}
 
+	item := Item{}
 	err = dynamodbattribute.UnmarshalMap(result.Item, &item)
 
 	return events.APIGatewayProxyResponse{
-		Body:       "Hello " + request.Body,
+		Body:       "Question: " + item.Question + ", Answer: " + item.Answer,
 		StatusCode: 200,
 	}, nil
 
